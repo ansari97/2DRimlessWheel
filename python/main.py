52def19196b87231f6645ebe1cb2c6ddcb27999c
Ahmed Ansari <ahmedalam97@gmail.com>
2024-09-28 22:07:41 -0500

    formatted main.py

--- python/main.py
+++ python/main.py
@@ -6,7 +6,7 @@ This file contains the main code for defining the dynamics of a 2D rimless wheel
 import math
 
 # Define paramaters
-slope_angle = 2 #degrees
-slope_angle = math.radians(slope_angle) #angle in radians
-m = 2   #mass in kg
-I = 1   #moment of inertia about center of mass/center of wheel in kgm^2
+slope_angle = 2  # degrees
+slope_angle = math.radians(slope_angle)  # angle in radians
+m = 2  # mass in kg
+I = 1  # moment of inertia about center of mass/center of wheel in kgm^2
