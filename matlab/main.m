31346d228b559259d59951772e1f35a72b138f8c
Ahmed Ansari <ahmedalam97@gmail.com>
2024-09-28 20:56:19 -0500

    added main.m file

--- matlab/main.m
+++ matlab/main.m
@@ -1,1 +1,7 @@
 % matlab file
+
+% Defining parameters
+slope_angle = 2; %degrees
+slope_angle = deg2rad(slope_angle); %angle in radians
+m = 2;   %mass in kg
+I = 1;   %moment of inertia about center of mass/center of wheel in kgm^2
