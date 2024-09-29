10de32fe4a03e8e5583a04aa602be814815005d5
Ahmed Ansari <ahmedalam97@gmail.com>
2024-09-28 21:45:28 -0500

    Revert "changed values in main.py"
    This reverts commit 88f5ff66536ea616dd098911eebc263feb163429.

--- python/main.py
+++ python/main.py
@@ -9,3 +9,4 @@ import math
 slope_angle = 2 #degrees
 slope_angle = math.radians(slope_angle) #angle in radians
 m = 2   #mass in kg
+I = 1   #moment of inertia about center of mass/center of wheel in kgm^2
